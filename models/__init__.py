8bcd7df9bafb382d9244f526dba7a777a03dc299
partychu <ytorres667@gmail.com>
2021-12-25 19:49:59 +0000

    Fixed variable

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""This module instantiates an object of class FileStorage or DBStorage"""
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from os import getenv
 
-if getenv('HBNB_MYSQL_DB') == 'db':
+if getenv('HBNB_TYPE_DB') == 'db':
     storage = DBStorage()
     storage.reload()
 
