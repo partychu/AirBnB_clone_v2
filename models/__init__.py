3b73b5875c88e8b746e6f0cc3bc26ada081a7e63
partychu <ytorres667@gmail.com>
2021-12-25 18:37:12 +0000

    Fix typo

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
-from models.models.db_storage import DBStorage
+from models.engine.db_storage import DBStorage
 from os import getenv
 
 if getenv('HBNB_MYSQL_DB') == 'db':
