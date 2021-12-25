5dd53cb66a4c9958904c52b787ca677138ffec9b
partychu <ytorres667@gmail.com>
2021-12-25 19:51:53 +0000

    Fix variable

--- models/state.py
+++ models/state.py
@@ -12,7 +12,7 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete")
 
-    if getenv('HBNB_MYSQL_DB') != 'db':
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
             """Getter"""
