02a1ca034b8686bf6ea9ef9de7a2dfd69caf2223
partychu <ytorres667@gmail.com>
2022-02-05 03:54:28 +0000

    Checking 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,8 +5,7 @@ sudo apt upgrade -y
 sudo apt-get install nginx -y
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared
-sudo touch index.html /data/web_static/releases/test/
-sudo echo "Success {OK}" | sudo tee data/web_static/releases/test
+sudo echo "Success {OK}" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
